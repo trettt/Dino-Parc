127a058c1bb716d32d9b7e87cb787a95224372e3
Bianca <spbiancag@gmail.com>
2024-04-09 10:09:04 +0300

    Main Menu
    added main menu

---
+++ Dino Parc/Assets/Scripts/LoadSceneButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; // Asigură-te că ai această linie pentru a folosi SceneManager
+
+public class LoadSceneButton : MonoBehaviour
+{
+    public void LoadSceneByName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
