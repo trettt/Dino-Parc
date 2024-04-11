ac7f55c0b9631f1d3a1c5b524bc465474a338380
Tret Elena <elena.tret@student.unitbv.ro>
2024-04-11 22:20:46 +0300

    added a simulation for the volcano

---
+++ Dino Parc/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    // Durata shake-ului
+    private float shakeDuration = 0f;
+
+    // Amplitudinea shake-ului
+    private float shakeAmount = 0.7f;
+
+    // Factorul de reducere a shake-ului în timp
+    private float decreaseFactor = 1.0f;
+
+    // Poziția originală a camerei
+    private Vector3 originalPos;
+
+    private void Start()
+    {
+        originalPos = transform.localPosition;
+    }
+
+    private void Update()
+    {
+        if (shakeDuration > 0)
+        {
+            // Generează o valoare de shake pe axa X
+            float shakeX = Random.Range(-1f, 1f) * shakeAmount;
+
+            // Aplică shake-ul pe axa X
+            transform.localPosition = new Vector3(originalPos.x + shakeX, originalPos.y, originalPos.z);
+
+            // Scade durata shake-ului
+            shakeDuration -= Time.deltaTime * decreaseFactor;
+        }
+        else
+        {
+            // Resetează poziția camerei la cea originală
+            shakeDuration = 0f;
+            transform.localPosition = originalPos;
+        }
+    }
+
+    // Funcție pentru a porni shake-ul camerei
+    public void Shake(float duration, float amount)
+    {
+        shakeDuration = duration;
+        shakeAmount = amount;
+    }
+}
---
+++ Dino Parc/Assets/Scripts/VolcanoSimulation.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolcanoSimulation : MonoBehaviour
+{
+    [SerializeField] private float detectionRadius;
+    [SerializeField] private AudioSource lavaSound;
+
+    private string message = "PRESS E TO START THE SIMULATION";
+    private bool isHovering = false;
+    private bool isSimulationActive = false;
+    private Transform player;
+    private float distanceToPlayer;
+
+    private ParticleSystem smokeParticles;
+    private ParticleSystem lavaParticles;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        lavaSound = GetComponent<AudioSource>();
+
+        smokeParticles = GameObject.Find("Smoke").GetComponent<ParticleSystem>();
+        lavaParticles = GameObject.Find("Lava").GetComponent<ParticleSystem>();
+
+        lavaParticles.Stop();
+        lavaSound.Stop();
+    }
+
+    private void Update()
+    {
+        distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        isHovering = distanceToPlayer <= detectionRadius;
+
+        if (isHovering && Input.GetKeyDown(KeyCode.E) && !isSimulationActive)
+        {
+            smokeParticles.Stop();
+            StartSimulation();
+            message = "";
+        }
+
+    
+    }
+
+    private void StartSimulation()
+    {
+        isSimulationActive = true;
+
+        Invoke("StartLavaFlow", 5f);
+
+        Invoke("StopSimulation", 15f);
+
+        Invoke("ShowMessage", 15f);
+
+        Camera.main.GetComponent<CameraShake>().Shake(15f, 0.3f);
+    }
+
+    private void StartLavaFlow()
+    {
+        lavaParticles.Play();
+        lavaSound.Play();
+    }
+
+    private void StopSimulation()
+    {
+        lavaParticles.Stop();
+        lavaSound.Stop();
+        smokeParticles.Play();
+        isSimulationActive = false;
+    }
+
+    private void ShowMessage()
+    {
+        message = "PRESS E TO START THE SIMULATION";
+    }
+
+    private void OnGUI()
+    {
+        if (isHovering && message != "")
+        {
+            GUIStyle style = new GUIStyle(GUI.skin.label);
+            style.fontSize = 20;
+            style.normal.textColor = Color.black;
+            style.fontStyle = FontStyle.Bold;
+            style.alignment = TextAnchor.MiddleCenter;
+
+            GUI.Label(new Rect(0, Screen.height - 50, Screen.width, 50), message, style);
+        }
+    }
+
+}
