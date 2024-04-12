d08aaafeeb1091c6126a80ee71997c7b7a499049
Bianca <spbiancag@gmail.com>
2024-04-12 13:35:54 +0300

    Added all dinosaurs and their info.

---
+++ Dino Parc/Assets/Scripts/SignpostInteraction.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class SignpostInteraction : MonoBehaviour
+{
+    [SerializeField] private float detectionRadius;
+    [SerializeField] private GameObject infoPanel; 
+
+    private string message = "PRESS I TO READ MORE";
+    private bool isHovering = false;
+    private Transform player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        
+        infoPanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        isHovering = distanceToPlayer <= detectionRadius;
+
+        if (isHovering && Input.GetKeyDown(KeyCode.I))
+        {
+            ToggleInfoPanel();
+        }
+    }
+
+    private void ToggleInfoPanel()
+    {
+       
+        infoPanel.SetActive(!infoPanel.activeSelf);
+    }
+
+    private void OnGUI()
+    {
+        if (isHovering && !infoPanel.activeSelf)
+        {
+            GUIStyle style = new GUIStyle(GUI.skin.label);
+            style.fontSize = 20;
+            style.normal.textColor = Color.white;
+            style.fontStyle = FontStyle.Bold;
+            style.alignment = TextAnchor.MiddleCenter;
+
+            GUI.Label(new Rect(0, Screen.height - 50, Screen.width, 50), message, style);
+        }
+    }
+}
